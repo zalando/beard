3660bda638e411cf0b2367ab3103deef17a9ed7c
Dan Persa <dan.persa@gmail.com>
2015-09-03 18:50:38 +0200

    Precompile the templates for the performance test

--- src/test/scala/de/zalando/beard/performance/BeardPerformanceTest.scala
+++ src/test/scala/de/zalando/beard/performance/BeardPerformanceTest.scala
@@ -2,7 +2,7 @@ package de.zalando.beard.performance
 
 import de.zalando.beard.parser.BeardTemplateParser
 import de.zalando.beard.performance.Time._
-import de.zalando.beard.renderer.{DefaultTemplateCompiler, BeardTemplateRenderer}
+import de.zalando.beard.renderer.{TemplateName, DefaultTemplateCompiler, BeardTemplateRenderer}
 import org.scalatest.{FunSpec, Matchers}
 
 import scala.io.Source
@@ -19,8 +19,12 @@ class BeardPerformanceTest extends FunSpec with Matchers {
 
     it ("should render the beard template") {
       val template = BeardTemplateParser {
-        Source.fromInputStream(getClass.getResourceAsStream(s"/templates/layout-with-partial.beard")).mkString
+        Source.fromInputStream(getClass.getResourceAsStream("/templates/layout-with-partial.beard")).mkString
       }
+
+      compiler.compile(TemplateName("/templates/layout-with-partial.beard"))
+      compiler.compile(TemplateName("/templates/_footer.beard"))
+      compiler.compile(TemplateName("/templates/_partial.beard"))
 
       val context: Map[String, Map[String, Object]] = Map("example" -> Map("title" -> "Title", "presentations" ->
         Seq(Map("title" -> "Title1", "speakerName" -> "Name1", "summary" -> "Summary1"),
